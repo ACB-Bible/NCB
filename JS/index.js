0d92770aea27ebbaa24be383a884f985169ab9fd
ACB <ACBBible@gmail.com>
2023-10-28 13:47:06 -0400

    Fixed Data and Launch.json

--- JS/index.js
+++ JS/index.js
@@ -99,7 +99,6 @@ async function ncbLoadVersions() {
 
     versions.forEach(version => {
         // #region load the default version dropdown box
-        if ( version.Selected === 1 ) {
         let div = document.createElement("div");
         div.addEventListener('click', function (event) {
             event.stopPropagation();
@@ -132,7 +131,6 @@ async function ncbLoadVersions() {
         div.classList.add('cs-ncbVersion');
         div.classList.add('cs-ncbChangeVersionSelect');
         document.getElementById('id-ncbChangeVersion').appendChild(div);
-    };
         // #endregion load the change version dropdown box
     });
 
